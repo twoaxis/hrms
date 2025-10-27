3b2514e7ec3179eb4590d3e5961f571efe72d207
Ahmed Helmy <ahmedmohammed18112003@gmail.com>
2025-10-27 19:24:42 +0300

    change the package name of the app.

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.hrms"
+    namespace = "org.twoaxis.hrms"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,10 +20,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.hrms"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "org.twoaxis.hrms"
+
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
--- mobile/android/app/src/main/kotlin/com/example/hrms/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.hrms
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
